a4a2648a09218c651d1a9dc73ad4ef6b71293f0d
OctavioRdz17 <octavio.rdzrojas@gmail.com>
2024-03-09 14:29:56 -0600

    Update connection type in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on oyur smoothie will be: '+name_on_order)
 
-cnx = st.connection('snowflake')
+cnx = st.connection("snowflake",type='snowflake')
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -42,6 +42,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!  '+name_on_order+'.',icon="✅")
     
-
 
 
